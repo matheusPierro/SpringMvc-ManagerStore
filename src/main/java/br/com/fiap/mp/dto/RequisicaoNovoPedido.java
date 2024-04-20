43a517243064cff5ddafab86044efcaf65615692
matheusPierro <matpierro570@gmail.com>
2024-04-19 23:22:20 -0300

    Feat: big commit with the project

---
+++ src/main/java/br/com/fiap/mp/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,17 @@
+package br.com.fiap.mp.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RequisicaoNovoPedido(
+		@NotBlank
+		@Size(min = 5, max = 250, message="quantidade mínima 5 e máxima 250 caracteres")
+		String nomeProduto,
+		@NotBlank
+		String urlProduto,
+		@NotBlank
+		String urlImagem,
+		String descricao		
+	) {
+
+}
---
+++ src/main/java/br/com/fiap/mp/model/StatusPedido.java
@@ -0,0 +1,7 @@
+package br.com.fiap.mp.model;
+
+public enum StatusPedido {
+	AGUARDANDO,
+	APROVADO,
+	ENTREGUE
+}
